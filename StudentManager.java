b2febb47df898e0f7d68675f7b3e4677196cf85b
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 22:23:57 -0500

    Create StudentManager.java

---
+++ StudentManager.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+
+/**
+ * The StudentManager class stores information about all Students and can find a
+ * Student when given the Student's pid.
+ * 
+ * @author Taimoor Qamar
+ * @version 10/5/19
+ */
+
+public class StudentManager {
+    private DetailedStudent[] sortedArray;
+    private SaveAndLoad saveAndLoad;
+
+    /**
+     * Checks to see if any data has been loaded into the StudentManager
+     * 
+     * @return whether data has been loaded
+     */
+    public boolean isInitialized() {
+        return sortedArray != null;
+    }
+
+    /**
+     * Resets all data in StudentManager to how it was at the beginning.
+     */
+    public void clear() {
+        sortedArray = null;
+        saveAndLoad = null;
+    }
+
+    /**
+     * Returns the Student with the given pid or null if not found
+     * 
+     * @param pid
+     *            the pid of the Student to find
+     * @return the found Student or null or not found
+     */
+    public Student find(String pid) {
+        if (isInitialized()) {
+            return findHelp(pid, -1, sortedArray.length);
+        }
+        else {
+            System.out.println("Student Manager: find: Nothing to find");
+            return null;
+        }
+    }
+
+    /**
+     * Binary Search Algorithm
+     * 
+     * @param pid
+     *            looking for element with this pid
+     * @param min
+     *            we know the element's index is either > to this index or the
+     *            element is not in the array
+     * @param max
+     *            we know the element's index is either < to this index or the
+     *            element is not in the array
+     * @return found DetailedStudent or null if not found
+     */
+    private DetailedStudent findHelp(String pid, int min, int max) {
+        if (max - min < 2) {
+            return null;
+        }
+        if (sortedArray[(min + max) / 2].getPID().compareTo(pid) > 0) {
+            return findHelp(pid, min, (min + max) / 2);
+        }
+        if (sortedArray[(min + max) / 2].getPID().compareTo(pid) < 0) {
+            return findHelp(pid, (min + max) / 2, max);
+        }
+        if (sortedArray[(min + max) / 2].getPID().compareTo(pid) == 0) {
+            return sortedArray[(min + max) / 2];
+        }
+        return null;
+    }
+
+    /**
+     * Loads a .data or .csv file with the given filename (given filename must
+     * include
+     * .data or .csv) that stores all of the information in this StudentManager.
+     * 
+     * @param filename
+     *            file from which to load
+     */
+    public void load(String fileName) {
+        SaveAndLoad load = new SaveAndLoad(fileName);
+        sortedArray = load.loadStudentData();
+        Arrays.sort(sortedArray);
+    }
+}
